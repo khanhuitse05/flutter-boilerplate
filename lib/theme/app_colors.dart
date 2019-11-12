48f7d9df28f77a6bec5f89a2c06bf1dd9b50f24d
PingAK <khanhuitse05@gmail.com>
2019-11-12 15:36:07 +0700

    +add dark theme

---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+
+
+}
+
+class DarkColor{
+  // example dark mode
+  final primaryColor = Colors.purple[200];
+  final primaryVariantColor = Colors.blue[700];
+  final secondaryColor = Colors.teal[200];
+  final secondaryVariantColor = Colors.teal[200];
+  final backgroundColor = Color(0xff121212);
+  final surfaceColor = Color(0xff1e1e1e);
+  final errorColor = Color(0xffcf6679);
+
+  final onPrimaryColor = Colors.black;
+  final onSecondaryColor= Colors.black;
+  final onBackgroundColor = Colors.white;
+  final onSurfaceColor = Colors.white;
+  final onErrorColor = Colors.black;
+
+  ///  Disabled states
+  // Label/Icon: 38% White
+  // Container: Outlined: 12% White or Filled: 12% White
+}
--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -4,18 +4,11 @@ import 'package:chat_app/core/storage_manager.dart';
 import 'package:flutter/material.dart';
 
 class ThemeManager {
-
-  List<ThemeData> get _appThemes => [
-        primaryTheme(),
-        secondaryTheme(),
-      ];
-
   int _currentTheme = 0;
 
   StreamController<ThemeData> _themeController = StreamController<ThemeData>();
 
   Stream<ThemeData> get theme => _themeController.stream;
-  StreamController<int> themeIndex = StreamController<int>();
 
   ThemeManager() {
     loadUserTheme();
@@ -33,22 +26,80 @@ class ThemeManager {
   changeTheme(int index) async {
     _currentTheme = index;
 
-    var userTheme = _appThemes[_currentTheme];
-    _themeController.add(userTheme);
-    themeIndex.add(_currentTheme);
+    _themeController
+        .add(_currentTheme == 0 ? primaryTheme() : secondaryTheme());
     await StorageManager.setObject("user_theme", index);
   }
 
-
   ThemeData primaryTheme() {
     return ThemeData(
-      primarySwatch: Colors.blue,
-    );
+        primarySwatch: Colors.blue,
+
+        /// Support swipe from edge to navigate the previous scene
+        /// for both iOS and android
+        pageTransitionsTheme: PageTransitionsTheme(
+          builders: {
+            TargetPlatform.android: CupertinoPageTransitionsBuilder(),
+            TargetPlatform.iOS: CupertinoPageTransitionsBuilder(),
+          },
+        ));
   }
 
+  // Example Dark mode
   ThemeData secondaryTheme() {
     return ThemeData(
-      primarySwatch: Colors.red,
+      brightness: Brightness.dark,
+
+      /// The background color for major parts of the app (toolbars, tab bars, etc)
+      primaryColor: Colors.purple[200],
+      primaryColorBrightness: Brightness.light,
+      canvasColor: Colors.white,
+
+      /// Accent color is also known as the secondary color.
+      accentColor: Colors.teal[200],
+      accentColorBrightness: Brightness.dark,
+      backgroundColor: Color(0xff121212),
+
+      /// The background color for a typical material app or a page within the app.
+      scaffoldBackgroundColor: Color(0xff121212),
+      bottomAppBarColor: Color(0xff1e1e1e),
+
+      /// The color of [Material] when it is used as a [Card].
+      cardColor: Colors.white12,
+      highlightColor: Colors.teal[200],
+      splashColor: Color(0xff121212),
+      disabledColor: Colors.white12,
+      dialogBackgroundColor: Color(0xff1e1e1e),
+      errorColor: Color(0xffcf6679),
+
+      /// App bar theme
+      appBarTheme: AppBarTheme(
+          textTheme: TextTheme(
+              title: TextStyle(color: Colors.white),
+              caption: TextStyle(color: Colors.white)),
+          actionsIconTheme: IconThemeData(
+            color: Colors.white,
+          ),
+          iconTheme: IconThemeData(
+            color: Colors.white,
+          )),
+
+      /// Icon theme
+      iconTheme: IconThemeData(
+        color: Colors.white,
+      ),
+      primaryIconTheme: IconThemeData(
+        color: Colors.white,
+      ),
+
+      /// Support swipe from edge to navigate the previous scene
+      /// for both iOS and android
+      pageTransitionsTheme: PageTransitionsTheme(
+        builders: {
+          TargetPlatform.android: CupertinoPageTransitionsBuilder(),
+          TargetPlatform.iOS: CupertinoPageTransitionsBuilder(),
+        },
+      ),
     );
   }
 }
