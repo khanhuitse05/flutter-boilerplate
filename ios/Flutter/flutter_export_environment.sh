c7adcee780b936143caaedb01ec1661fbc43c37f
j1mmyto9 <khanhuitse05@gmail.com>
2021-06-19 15:35:11 +0700

    upgrade

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/khanhuitse05/Development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/khanhuitse05/Documents/FlutterProject/init-flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/khanhuitse05/Documents/FlutterProject/flutter-boilerplate"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
--- lib/_devmode/widget/float_dev_button.dart
+++ lib/_devmode/widget/float_dev_button.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:my_app/_devmode/view/dev_screen.dart';
-// ignore: import_of_legacy_library_into_null_safe
-import 'package:flutter_icons/flutter_icons.dart';
 
 class FloatDevButton extends StatelessWidget {
   @override
@@ -10,7 +8,7 @@ class FloatDevButton extends StatelessWidget {
     return FloatingActionButton(
       key: const Key("FloatDevButton"),
       onPressed: () => Get.to(DevScreen()),
-      child: const Icon(MaterialCommunityIcons.access_point),
+      child: const Icon(Icons.ac_unit),
     );
   }
 }
