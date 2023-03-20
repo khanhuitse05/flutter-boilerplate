4ee699e8c07ee94efaa2a440f82762a092a884c3
j1mmyto9 <khanhuitse05@gmail.com>
2023-03-20 12:22:06 +0700

    upgra native folder

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/khanhuitse05/Developer/flutter_latest"
-export "FLUTTER_APPLICATION_PATH=/Users/khanhuitse05/Documents/FlutterProject/flutter-boilerplate"
+export "FLUTTER_APPLICATION_PATH=/Users/khanhuitse05/Documents/FlutterProject/flutter-boilerplate/my_app"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
